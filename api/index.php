b89281655b99c8e32e592538ed71a2e3e46d6972
Hilya <sendtohilya@gmail.com>
2026-06-13 14:44:02 +0700

    repair providers

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
+
+$directories = [
+    '/tmp/storage/logs',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/sessions',
+    '/tmp/bootstrap/cache'
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
